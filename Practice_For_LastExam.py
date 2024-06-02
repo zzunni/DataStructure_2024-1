2608895b8f36bce1a2bb4e53fecefa5ec83a1c67
zzunni <12201309@inha.edu>
2024-06-02 23:49:54 +0900

    Tree-4_levelorder

--- Practice_For_LastExam.py
+++ Practice_For_LastExam.py
@@ -39,3 +39,25 @@ class BinaryTree:
                 return lheight + 1
             else:
                 return rheight + 1
+   def levelOrder(self):
+        # 루트 노드의 높이를 구한 다음 높이가 1부터 h까지 순차적으로 노드를 구한다.
+        h = self.height(self.root)
+        for i in range(1, h + 1): # 높이가 1, 2, ... , h 까지 순차적으로 프린트한다. 루트의 높이는 1이고, 바로 밑은 높이가 2다.
+            self._levelOrder(self.root, i)
+            print()
+
+    # 특정 노드의 레벨에 해당하는 노드를 프린트한다.
+    # 예: 루트에서 레벨 2를 프린트한다면 레벨을 한 단계 낮춰 루트 좌우로 이동한다.
+    #     이후, 레벨 1이 되므로 루트의 좌, 우 노드가 프린트 된다.
+    #     레벨 3을 프린트 한다면 레벨을 한 단계 낮춘 상태 즉 루트 바로 밑을 루트로 보고 재귀적으로 레벨 2를 수행하는 것이다.
+
+    def _levelOrder(self, node, level):
+        if node is None:
+            return
+        # 특정 노드의 level == 1일 때, 특정 노드 값을 인쇄한다.
+        if level == 1:
+            print(node.item, end = " ")
+        # level > 1 이면 특정 노드의 좌, 우측으로 이동해서 레벨을 다운시켜 진행한다.
+        elif level > 1:
+            self._levelOrder(node.left, level - 1)
+            self._levelOrder(node.right, level - 1)
