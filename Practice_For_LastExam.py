0803d090560bc00bf9b20bcf50f973fec0b8bae9
zzunni <12201309@inha.edu>
2024-06-02 22:59:59 +0900

    Tree-4_ãheight

--- Practice_For_LastExam.py
+++ Practice_For_LastExam.py
@@ -1,18 +1,3 @@
-class BNode:
-    def __init__(self, item):
-        # 노드의 초기화를 담당하는 생성자 메서드
-        self.item = item  # 노드가 저장하는 값
-        self.left = None  # 왼쪽 자식 노드를 가리키는 포인터
-        self.right = None  # 오른쪽 자식 노드를 가리키는 포인터
-
-    def setLeft(self, node):
-        # 왼쪽 자식 노드를 설정하는 메서드
-        self.left = node
-
-    def setRight(self, node):
-        # 오른쪽 자식 노드를 설정하는 메서드
-        self.right = node
-
 class BinaryTree:
     def __init__(self, root):
         # 트리의 초기화를 담당하는 생성자 메서드
@@ -42,42 +27,15 @@ class BinaryTree:
             self.postOrder(n.right)  # 오른쪽 자식을 재귀적으로 방문
         print(n.item, ' ', end=' ')  # 현재 노드를 마지막에 방문
 
-# 노드들을 생성하고 각 노드에 값을 할당
-a = BNode('A')
-b = BNode('B')
-c = BNode('C')
-d = BNode('D')
-e = BNode('E')
-f = BNode('F')
-g = BNode('G')
-h = BNode('H')
-i = BNode('I')
-j = BNode('J')
-k = BNode('K')
-
-# 각 노드를 트리에 연결하여 구조를 형성
-a.setLeft(b)
-a.setRight(c)
-b.setLeft(d)
-b.setRight(e)
-c.setLeft(f)
-c.setRight(g)
-d.setLeft(h)
-e.setLeft(i)
-e.setRight(j)
-g.setRight(k)
-
-# 트리를 생성하고 루트 노드를 A로 설정
-bt = BinaryTree(a)
-
-# 전위 순회를 실행하고 결과를 출력
-bt.preOrder(a)
-print()  # 결과를 구분하기 위해 줄 바꿈
-
-# 중위 순회를 실행하고 결과를 출력
-bt.inOrder(a)
-print()  # 결과를 구분하기 위해 줄 바꿈
-
-# 후위 순회를 실행하고 결과를 출력
-bt.postOrder(a)
-print()  # 결과를 구분하기 위해 줄 바꿈
+    def height(self, n):
+        # 트리의 높이를 계산하는 메서드
+        if n is None:
+            return 0  # 터미널 노드를 만나면 0을 리턴
+        else:
+            lheight = self.height(n.left)  # 왼쪽 서브트리의 높이 계산
+            rheight = self.height(n.right)  # 오른쪽 서브트리의 높이 계산
+            # 왼쪽과 오른쪽 서브트리 중 더 큰 높이에 1을 더한 값이 현재 노드의 높이
+            if lheight > rheight:
+                return lheight + 1
+            else:
+                return rheight + 1
